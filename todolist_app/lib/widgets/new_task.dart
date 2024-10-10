485debb58baab71be68f08729238cdde2f765e9a
Safae Moutaouakil <safae.moutaouakil@esi.ac.ma>
2024-10-10 16:29:15 +0100

    commit

---
+++ todolist_app/lib/widgets/new_task.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class NewTask extends StatefulWidget {
+  const NewTask({super.key});
+
+  @override
+  State<NewTask> createState() {
+    return _NewTaskState();
+  }
+}
+
+class _NewTaskState extends State<NewTask> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  void _submitTaskData() {
+    if (_titleController.text.trim().isEmpty) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Erreur'),
+          content: const Text(
+              'Merci de saisir le titre de la tâche à ajouter dans la liste'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('Okay'),
+            ),
+          ],
+        ),
+      );
+      return;
+    }
+
+    print(_titleController.text); // Code pour ajouter la tâche (à implémenter)
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text('Task title'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: _submitTaskData,
+                child: const Text('Enregistrer'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
